dbc43e3b42e153037be788d228a6014881c25b4e
breadgrocery <breadgrocery@gmail.com>
2025-12-26 10:32:21 +0800

    chore: 修改内置流程

--- src/config.ts
+++ src/config.ts
@@ -3,15 +3,15 @@ import { getNextDay4AM, getNextMonday4AM, useStoreWithDefaults } from "@bettergi
 //! 用户脚本设置
 export const userConfig = {
   //! 每周任务相关设置
-  room: settings.room || "20134075027",
-  playbacks: (settings.playbacks || "通关回放1.json,通关回放2.json")
+  room: settings.room || "20031486040",
+  playbacks: (settings.playbacks || "美景音乐播放器结算.json")
     .replace(/，/g, ",")
     .split(",")
     .map(str => str.trim())
     .filter(Boolean),
   expPerAttempt: Math.max(1, Number(settings.expPerAttempt || "20")),
   deleteStageSave: settings.deleteStageSave ?? false,
-  deleteStageSaveKeyword: settings.deleteStageSaveKeyword || "深渊100层",
+  deleteStageSaveKeyword: settings.deleteStageSaveKeyword || "音乐播放",
   expWeeklyLimit: Math.max(1, Number(settings.expWeeklyLimit || "4000")),
   force: settings.force ?? false,
   thisAttempts: Math.max(0, Number(settings.thisAttempts || "0")),
@@ -22,7 +22,7 @@ export const userConfig = {
     .split(",")
     .map(str => str.trim())
     .filter(Boolean),
-  dailyPlaybacks: (settings.dailyPlaybacks || "通关回放1.json,通关回放2.json;60秒按1通关.json")
+  dailyPlaybacks: (settings.dailyPlaybacks || "通关回放1.json,通关回放2.json;40秒按1通关.json")
     .replace(/，/g, ",")
     .replace(/；/g, ";")
     .split(";")
